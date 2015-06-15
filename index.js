d1492f9447786a3660faf0df67de99e26472a696
Jean-Christophe Hoelt <hoelt@fovea.cc>
2015-06-15 17:45:56 +0300

    Change endpoint to be /admin/v1/...

--- index.js
+++ index.js
@@ -10,16 +10,13 @@ var LocalStrategy = require('passport-local').Strategy;
 var bodyParser = require('body-parser');
 var flash = require("flash");
 
-var API_BASE_URL = process.env.API_BASE_URL || "https://ganomede-devel.fovea.cc";
+var API_BASE_URL = process.env.API_BASE_URL || "https://staging.ggs.ovh";
 var API_TEMP_URL = process.env.API_TEMP_URL || "http://private-194a93-ganomedeadmin.apiary-mock.com";
 var API_CHECKPOINTS_URL = process.env.API_CHECKPOINTS_URL || "http://192.168.59.103" || "http://zalka.fovea.cc:49660";
 
 
 var users = [
-	{username: "jad", password: "jad"},
-	{username: "test", password: "test"},
-	{username: "1", password: "1"},
-	{username: "2", password: "2"}
+	{ username: process.env.ADMIN_USERNAME, password: process.env.ADMIN_PASSWORD }
 ];
 var tokens = {};
 
@@ -31,6 +28,7 @@ var sendNeedAuth = function (res) {
    });
 };
 
+var apiBase = "/admin/v1";
 
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
@@ -74,7 +72,7 @@ var auth = function(req, res, next){
         next();
       });
     });
-}; 
+};
 
 function issueToken(user, done) {
   var token = utils.generateToken(64);
@@ -85,105 +83,102 @@ function issueToken(user, done) {
 }
 
 
-app.post('/api/login', 
-	passport.authenticate('local'),
-	function(req, res, next) {
-		 issueToken({username: req.body.username, password:req.body.password}, function(err, token) {
-	      if (err) { return next(err); }
-	      res.cookie('token', token, { path: '/', httpOnly: true, maxAge: 604800000 });
-	      res.send({success: true});
-	      // return next();
+app.post(apiBase + '/api/login', passport.authenticate('local'), function(req, res, next) {
+    if (res.headerSent) { return; }
+		 issueToken({
+             username: req.body.username,
+             password:req.body.password
+         }, function(err, token) {
+             if (err) {
+                 return next(err);
+             }
+             res.cookie('token', token, { path: '/', httpOnly: true, maxAge: 604800000 });
+             res.send({ success: true });
+             // return next();
     	});
-  },
-  function(req, res) {
-  	res.redirect('/admin/');
-  }
+    },
+    function(req, res) {
+        res.redirect('/admin/');
+    }
  );
 
 app.get(/avatars\/v1\/(.+)$/, auth, function(req, res) {
 	request.get(API_BASE_URL + req.url).pipe(res);
 });
 
-/* serves main page */
- app.get("/", function(req, res) {
- 	res.writeHeader(304, {
-
- 	});
- });
-
- app.get("/api/logout", auth, function(req, res){
+ app.get(apiBase + "/api/logout", auth, function(req, res){
  	utils.removeToken(utils.parseCookies(req).token, tokens);
  	res.clearCookie('token');
  	res.redirect('/admin/');
  });
 
 //get users list
- app.get("/api/users", auth, function(req, res){
- 	request(API_TEMP_URL + req.url).pipe(res);
- });
+app.get(apiBase + "/api/users", auth, function(req, res){
+    request(API_TEMP_URL + "/api/users").pipe(res);
+});
 
- //get user details
- app.get("/api/user/:id", auth, function(req, res){
- 	request(API_TEMP_URL + req.url).pipe(res);
- });
+//get user details
+app.get(apiBase + "/api/user/:id", auth, function(req, res){
+    request(API_TEMP_URL + req.url).pipe(res);
+});
 
-  //ban user
- app.post("/api/user/ban/:id", auth, function(req, res){
- 	request.post(API_TEMP_URL + req.url).pipe(res);
- });
+//ban user
+app.post(apiBase + "/api/user/ban/:id", auth, function(req, res){
+    request.post(API_TEMP_URL + req.url).pipe(res);
+});
 
- //get location
- app.get("/api/location/:id", auth, function(req, res){
- 	request(API_BASE_URL + '/users/v1/' + req.params.id +'/metadata/location').pipe(res);
- });
+//get location
+app.get(apiBase + "/api/location/:id", auth, function(req, res){
+    request(API_BASE_URL + '/users/v1/' + req.params.id +'/metadata/location').pipe(res);
+});
 
-  //get location
- app.get("/api/test/", auth, function(req, res){
-  console.log(req.params);
-  console.log(req.body);
-  res.send("ok");
- });
+//get location
+app.get(apiBase + "/api/test/", auth, function(req, res){
+    console.log(req.params);
+    console.log(req.body);
+    res.send("ok");
+});
 
 //get items list
- app.delete("/api/item/:id", auth, function(req, res){
- 	request.del(API_TEMP_URL + req.url).pipe(res);
- });
-
- app.get("/api/items/:name", auth, function(req, res){
- 	request(API_TEMP_URL + req.url).pipe(res);
- });
-
-app.get("/api/items", auth, function(req, res){
- 	request(API_TEMP_URL + req.url).pipe(res);
+app.delete(apiBase + "/api/item/:id", auth, function(req, res){
+    request.del(API_TEMP_URL + req.url).pipe(res);
 });
 
-app.put("/api/item/:id", auth, function(req, res){
-	request.put(API_TEMP_URL + req.url).pipe(res);
+app.get(apiBase + "/api/items/:name", auth, function(req, res){
+    request(API_TEMP_URL + "/api/items/" + req.params.name).pipe(res);
 });
 
-app.post("/api/item", auth, function(req, res){
+app.get(apiBase + "/api/items", auth, function(req, res){
+ 	request(API_TEMP_URL + "/api/items").pipe(res);
+});
+
+app.put(apiBase + "/api/item/:id", auth, function(req, res){
+	request.put(API_TEMP_URL + "/api/item/" + req.params.id).pipe(res);
+});
+
+app.post(apiBase + "/api/item", auth, function(req, res){
 	request.post(API_TEMP_URL + req.url).pipe(res);
 });
 
 //monitoring
-app.get("/api/monitoring", auth, function(req, res){
-  request(API_BASE_URL + "/registry/v1/services").pipe(res);
+app.get(apiBase + "/api/monitoring", auth, function(req, res){
+    request(API_BASE_URL + "/registry/v1/services").pipe(res);
 });
 
 
 app.get(/^\/checkpoints\/v1\/(.+)$/, auth, function(req, res){
-  request(API_CHECKPOINTS_URL + req.url).pipe(res);
+    request(API_CHECKPOINTS_URL + req.url).pipe(res);
 });
 
 /* serves main page */
- app.get("/admin/", function(req, res) {
+app.get(apiBase + "/admin/", function(req, res) {
     res.sendFile(__dirname + "/web/index.html");
- });
+});
 
 /* serves all the static files */
- app.get(/^\/admin\/(.+)$/, function(req, res) {
-     res.sendFile(__dirname + "/web/" + req.params[0]); 
- });
+app.get(/^\/admin\/v1\/web\/(.+)$/, function(req, res) {
+    res.sendFile(__dirname + "/web/" + req.params[0]);
+});
 
 var server = app.listen(3000, function () {
 
