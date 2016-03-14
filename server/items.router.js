403d64c621d9b1c9e33d20b7bc3df6f7852a69be
Alexey Zabrodsky <elmigranto@gmail.com>
2016-03-14 18:22:59 +0300

    items router uses actual virtual currency for listing items.

--- server/items.router.js
+++ server/items.router.js
@@ -1,29 +1,20 @@
 'use strict';
 
 const express = require('express');
-const request = require('request');
+const utils = require('./utils');
+const config = require('../config');
 
-const API_TEMP_URL = process.env.API_TEMP_URL || "http://private-194a93-ganomedeadmin.apiary-mock.com";
 const router = new express.Router();
 
-router.delete('/item/:id', function (req, res) {
-    request.del(API_TEMP_URL + req.url).pipe(res);
-});
+const pipeTo = (options) => {
+  return (req, res) => {
+    utils.safeRequestPipe[options.method](options.url, res);
+  };
+};
 
-router.get('/items/:name', function (req, res) {
-    request(`${API_TEMP_URL}/api/items/${req.params.name}`).pipe(res);
-});
-
-router.get('/items', function (req, res) {
-  request(`${API_TEMP_URL}/api/items`).pipe(res);
-});
-
-router.put('/item/:id', function (req, res) {
-  request.put(`${API_TEMP_URL}/api/item/${req.params.id}`).pipe(res);
-});
-
-router.post('/item', function (req, res) {
-  request.post(API_TEMP_URL + req.url).pipe(res);
-});
+router.get('/items', pipeTo({
+  method: 'get',
+  url: `${config.services.virtualcurrency}/auth/token/products`
+}));
 
 module.exports = router;
