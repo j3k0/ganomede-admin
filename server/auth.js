4d4bb46327795975b2b9b3f456c49aa99a68937c
Alexey Zabrodsky <elmigranto@gmail.com>
2016-04-06 17:07:18 +0300

    simplified auth:  - only allows single user with ADMIN_USERNAME  - token is based on hash of username+password

--- server/auth.js
+++ server/auth.js
@@ -8,75 +8,40 @@ const config = require('../config');
 
 const router = new express.Router();
 
-const users = {
-  store: (function () {
-    const ret = {};
-    const user = {
-      username: config.auth.admin.username,
-      password: config.auth.admin.password
-    };
-    ret[user.username] = user;
-    return ret;
-  }()),
-
-  findByUsername: function (username) {
-    return this.store.hasOwnProperty(username)
-      ? this.store[username]
-      : null;
-  }
+const toToken = function (username, password) {
+  return crypto
+    .createHash('sha256')
+    .update(username)
+    .update(password)
+    .digest('hex');
 };
 
-const tokens = {
-  store: (function () {
-    const ret = {};
-
-    if (config.auth.admin.token)
-      ret[config.auth.admin.token] = config.auth.admin.username;
-
-    return ret;
-  }()),
-
-  create: function (username) {
-    const token = crypto.randomBytes(32).toString('hex');
-    this.store[token] = username;
-    return token;
-  },
-
-  consume: function (token) {
-    return this.store.hasOwnProperty(token)
-      ? this.store[token]
-      : null;
-  },
-
-  remove: function (token) {
-    delete this.store[token];
-  }
-};
-
+const admin = config.auth.admin;
+const adminToken = toToken(admin.username, admin.password);
 
 passport.serializeUser(function (user, done) {
+  if (user.username !== admin.username)
+    return done(new Error('UknownUser'));
+
   done(null, user.username);
 });
 
 passport.deserializeUser(function (username, done) {
-  done(null, users.findByUsername(username));
+  if (username !== admin.username)
+    return done(new Error('UknownUser'));
+
+  done(null, admin);
 });
 
 passport.use(new LocalStrategy(function (username, password, done) {
-  const user = users.findByUsername(username);
-
-  return user && (user.password === password)
-    ? done(null, user)
+  return adminToken === toToken(username, password)
+    ? done(null, admin)
     : done(null, false, {message: 'Invalid username or password.'});
 }));
 
 
 const mwValidate = function(req, res, next){
-  const token = req.cookies && req.cookies.token;
-  const username = tokens.consume(token);
-  const user = users.findByUsername(username);
-
-  if (token && username && user)
+  if (req.cookies && (req.cookies.token === adminToken))
     return next();
 
   res.status(401).json({
@@ -87,13 +52,11 @@ const mwValidate = function(req, res, next){
 };
 
 router.post('/login', passport.authenticate('local'), function (req, res) {
-  const token = tokens.create(req.body.username);
-  res.cookie('token', token, {path: '/', httpOnly: true, maxAge: 604800000});
+  res.cookie('token', adminToken, {path: '/', httpOnly: true, maxAge: 604800000});
   res.json({success: true});
 });
 
 router.get('/logout', mwValidate, function (req, res){
-  tokens.remove(req.cookies.token);
   res.clearCookie('token');
   res.json({success: true});
 });
