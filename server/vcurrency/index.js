8d97b94c7b10d54b2a5d92f0b02b4a0571413af8
Alexey Zabrodsky <elmigranto@gmail.com>
2016-04-09 17:20:46 +0300

    generalized items.router to any single-list API of vcurrency (items, packs, etc.)

--- server/vcurrency/index.js
+++ server/vcurrency/index.js
@@ -1,9 +1,16 @@
 'use strict';
 
 const express = require('express');
+const proxy = require('./single-list-proxy');
+const config = require('../../config');
 
 const router = new express.Router();
 
-router.use('/items', require('./items.router'));
+router.use('/items', proxy('/products', (json) => {
+  return {
+    items: json,
+    currencies: config.services.virtualcurrency.currencies
+  };
+}));
 
 module.exports = router;
