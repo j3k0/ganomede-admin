d08f93c70f78254ab41da7f24b56331487850b06
Alexey Zabrodsky <elmigranto@gmail.com>
2017-10-22 22:04:37 +0500

    Die on `unhandledRejection`s.

--- index.js
+++ index.js
@@ -4,11 +4,35 @@ const config = require('./config');
 const app = require('./server/app');
 const log = require('./server/log');
 
-log('Running with config', config);
+const die = (title) => (exception) => {
+  log.error('****************')
+  log.error('*** CRITICAL ***')
+  log.error('****************')
+  log.error('');
+  log.error(title);
+  log.error('');
+  log.error(exception);
+  log.error('');
+  log.error('****************')
+  log.error('');
 
-const server = app.listen(config.http.port, config.http.host, function () {
-  const host = server.address().address;
-  const port = server.address().port;
+  process.exit(1);
+};
 
-  log('App listening at http://%s:%s', host, port);
-});
+const main = () => {
+  log('Running with config', config);
+
+  const server = app.listen(config.http.port, config.http.host, function () {
+    const host = server.address().address;
+    const port = server.address().port;
+
+    log('App listening at http://%s:%s', host, port);
+  });
+};
+
+if (!module.parent) {
+ process.on('uncaughtException', die('uncaughtException'));
+ process.on('unhandledRejection', die('unhandledRejection'));
+
+ main();
+}
