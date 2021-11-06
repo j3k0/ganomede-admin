6b51f967e2696f4c4310cbbdb49ff1fcc90c2545
Hussein Taha <husseintaha@devbox2.fovea.cc>
2021-11-06 04:13:31 +0000

    add tests for the new api added

--- tests/usermetas.test.js
+++ tests/usermetas.test.js
@@ -1,21 +1,108 @@
 'use strict';
 
+const {
+	assert
+} = require('chai');
 const Helper = require('../server/users/helpers');
+const router = require('../server/users/index');
 
-let userId = 'test1';
 
 describe('usermetas', async () => {
-    describe('#dynamicMetadata()', () => {
-      it('returns user metas based on a test user', async () => {
-        const helper = td.object(Helper);  
-  
-        td.when(helper.dynamicMetadata(userId, 'location,locale,auth', td.callback))
-          .thenCallback(null, 'test');
-  
-        
-      });
-   
-   
-    });
-  });
-  
+	describe('#dynamicMetadata()', () => {
+		it('returns user metas based on a test user', (done) => {
+			const userId = 'test1';
+			const metadataList = ['usermeta1', 'usermeta2'];
+			const helper = td.object(Helper);
+			const response = [{
+				id: "usermeta1",
+				value: ""
+			}, {
+				id: "usermeta2",
+				value: ""
+			}];
+
+			td.when(helper.dynamicMetadata(userId, metadataList.join(','), td.callback))
+				.thenCallback(null, response);
+
+			const res = td.object(['json']);
+			const req = {
+				params: {
+					username: userId
+				}
+			};
+			const next = function(err) {
+				try {
+					assert(err == undefined, 'there should not be an error');
+					td.verify(res.json(response));
+					done();
+				} catch (e) {
+					done(e);
+				}
+			};
+			router.getUsermetaMiddleware(helper, metadataList.join(','))(req, res, next);
+		});
+
+		it('checking metadatalist emtpy', (done) => {
+			const userId = 'test1';
+			const metadataList = [];
+			const helper = td.object(Helper);
+			const response = [{
+				id: "",
+				value: ""
+			}];
+
+			td.when(helper.dynamicMetadata(userId, metadataList.join(','), td.callback))
+				.thenCallback(null, response);
+
+			const res = td.object(['json']);
+			const req = {
+				params: {
+					username: userId
+				}
+			};
+			const next = function(err) {
+				try {
+					assert(err == undefined, 'there should not be an error');
+					td.verify(res.json(response));
+					done();
+				} catch (e) {
+					done(e);
+				}
+			};
+			router.getUsermetaMiddleware(helper, metadataList.join(','))(req, res, next);
+		});
+
+    it('Updating metadata', (done) => {
+			const userId = 'test1'; 
+      const metaKey = 'usermeta1';
+      const metaVal = 'from-tests';
+			const helper = td.object(Helper);
+			const response = {};
+
+			td.when(helper.updateDynamicUserMeta(userId, metaKey, metaVal, td.callback))
+				.thenCallback(null, response);
+
+			const res = td.object(['json']);
+			const req = {
+				params: {
+					username: userId, 
+				},
+        body: {
+          id: metaKey,
+          value: metaVal
+        }
+			};
+			const next = function(err) {
+				try {
+					assert(err == undefined, 'there should not be an error');
+					td.verify(res.json(response));
+					done();
+				} catch (e) {
+					done(e);
+				}
+			};
+			router.updateMetaDataMiddleWare(helper)(req, res, next);
+		});
+
+	});
+});
